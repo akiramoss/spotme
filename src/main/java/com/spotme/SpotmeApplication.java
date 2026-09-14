c4d47eda6b6434a7b431a0c29c0626cb651783c9
Iñaki Ramos <iramositurria@gmail.com>
2026-09-14 23:05:56 +0200

    feat(user): add User entity, JPA auditing, and PostgreSQL datasource config

--- src/main/java/com/spotme/SpotmeApplication.java
+++ src/main/java/com/spotme/SpotmeApplication.java
@@ -2,12 +2,12 @@ package com.spotme;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class SpotmeApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(SpotmeApplication.class, args);
     }
-
 }
